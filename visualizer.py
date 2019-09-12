687dd659bcd2c9474d4631b633f5ec8d42eff97a
emanuel <eschlake@uni-bremen.de>
2019-09-12 22:58:53 +0200

    Now takes dictionary as argument. Reaches pylint > 9.

--- visualizer.py
+++ visualizer.py
@@ -1,31 +1,35 @@
+"""Module do visualize results and potential"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualizer(psi, V, w, x_exp, sigma_x, xMin, xMax, nPoint, first, last):
+def visualizer(psi, potential, energies, x_exp, sigma_x, **params):
     """script to plot wavefunctinos, energies, expectations values and uncertainty"""
 
-    xnew=np.linspace(xMin,xMax,nPoint)
-    f, axs = plt.subplots(1,2)
-    for i in range(first-1,last):
+    xnew = np.linspace(params['x_min'], params['x_max'], params['n_point'])
+    fig, axs = plt.subplots(1, 2)
+    for i in range(params['first']-1, params['last']):
        #sym=blau, asym=rot
-       if i%2==0:
-          color='blue'
-       elif i%2==1:
-          color='red'
-       #psi plot um w geschifted und gestreckt/gestaucht
-       axs[0].plot(xnew, psi[i]/3+w[i], color=color, linewidth=1)
-       #transparenter grau ton für eigenwerte
-       axs[0].axhline(w[i], xmin=xMin, xmax=xMax, linewidth=.3, color=(0, 0, 0, 0.75))
-       axs[1].axhline(w[i], xmin=xMin, xmax=xMax, linewidth=.3, color=(0, 0, 0, 0.75))
-       axs[0].scatter(x_exp[i],w[i],marker='x', color='green', s=100)
-       axs[1].scatter(sigma_x[i], w[i], marker='+', color='red', s=100)
-    #plotting first to last
+        if i%2 == 0:
+            color = 'blue'
+        elif i%2 == 1:
+            color = 'red'
+         #psi plot um w geschifted und gestreckt/gestaucht
+        axs[0].plot(xnew, psi[i]/3+energies[i], color=color, linewidth=1)
+        #transparenter grau ton für eigenwerte
+        axs[0].axhline(energies[i], xmin=params['x_min'], xmax=params['x_max'],
+                       linewidth=.3, color=(0, 0, 0, 0.75))
+        axs[1].axhline(energies[i], xmin=params['x_min'], xmax=params['x_max'],
+                       linewidth=.3, color=(0, 0, 0, 0.75))
+        axs[0].scatter(x_exp[i], energies[i], marker='x', color='green', s=100)
+        axs[1].scatter(sigma_x[i], energies[i], marker='+', color='red', s=100)
+    #plotting params['first'] to params['last']
     #potentialplot
-    axs[0].plot(xnew, V(xnew),color='black', linewidth=1)
+    axs[0].plot(xnew, potential(xnew), color='black', linewidth=1)
     #eigenfunktionen plot
 
 
     #achsenlimits
-    axs[0].set_xlim(xMin,xMax)
-    axs[0].set_ylim(w[0]-.5,w[last]+.5)
+    axs[0].set_xlim(params['x_min'], params['x_max'])
+    axs[0].set_ylim(energies[0]-.5, energies[params['last']]+.5)
     return axs
