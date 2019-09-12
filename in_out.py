f361a2f32517dbc8fb788338448c05abcaf063a6
emanuel <eschlake@uni-bremen.de>
2019-09-12 22:56:50 +0200

    now returns dict instead of variables. Fixed code to reach pylinscore > 9. Added try-except block and exit.

--- in_out.py
+++ in_out.py
@@ -2,7 +2,10 @@
 Reading input and savin results
 """
 
+
+import sys
 import numpy as np
+
 
 def params():
     """
@@ -10,37 +13,55 @@ def params():
 
     inp: Path to input file
     """
-    inp=input("Please enter path to the input file if the file is not located in the same directory as the script file or if the filename is not \"schroedinger.inp\": ")
+    inp = input("""Please enter path to the input file if the file is not located in the same directory as the script file or if the filename is not \"schroedinger.inp\": """)
 
-    if inp=="":
-        inp="./schroedinger.inp"
+    if inp == "":
+        inp = "./schroedinger.inp"
     else:
-        inp=inp
+        inp = inp
+    data = {'mass': 0, 'x_min': 0, 'x_max': 0, 'n_point': 0, 'first': 0,
+            'last': 0, 'method': 0, 'pot': 0, 'x_sup': 0, 'y_sup': 0}
 
     with open(inp, 'r') as inp:
-       for line in inp:
-          if 'mass' in line:
-             M=float(np.fromstring(line, dtype=float, sep=' '))
-          elif 'xMin' in line:
-             xMin=np.fromstring(line, dtype=float, sep=' ')[0]
-             xMax=np.fromstring(line, dtype=float, sep=' ')[1]
-             nPoint=int(np.fromstring(line, dtype=float, sep=' ')[2])
-          elif 'first and last' in line:
-             first=np.fromstring(line, dtype=int, sep=' ')[0]
-             last=np.fromstring(line, dtype=int, sep=' ')[1]
-          elif 'type' in line:
-             method=line.split(' ')[0]
-          elif 'nr. of' in line:
-             pot=np.loadtxt(inp.readlines())
-             x=pot[:,0]
-             y=pot[:,1]
-    return M, xMin, xMax, nPoint, first, last, method, pot, x, y
+        try:
+            for line in inp:
+                if 'mass' in line:
+                    mass = float(np.fromstring(line, dtype=float, sep=' '))
+                    data['mass'] = mass
+                elif 'xMin' in line:
+                    x_min = np.fromstring(line, dtype=float, sep=' ')[0]
+                    x_max = np.fromstring(line, dtype=float, sep=' ')[1]
+                    n_point = int(np.fromstring(line, dtype=float, sep=' ')[2])
+                    data['x_min'] = x_min
+                    data['x_max'] = x_max
+                    data['n_point'] = n_point
+                elif 'first and last' in line:
+                    first = np.fromstring(line, dtype=int, sep=' ')[0]
+                    last = np.fromstring(line, dtype=int, sep=' ')[1]
+                    data['first'] = first
+                    data['last'] = last
+                elif 'type' in line:
+                    method = line.split(' ')[0]
+                    data['method'] = method
+                elif 'nr. of' in line:
+                    pot = np.loadtxt(inp.readlines())
+                    data['x_sup'] = pot[:, 0]
+                    data['y_sup'] = pot[:, 1]
+            return data
+        except OSError:
+            print("Input file couldn't be read.")
+            sys.exit(1)
 
-def output(potential, energies, wavefuncs, x_exp, sigma_x, xnew, first, last):
+
+
+def output(potential, energies, wavefuncs, x_exp, sigma_x, xnew, first, last):#toomanyarguments
     """
     Save potential, eigenvalues, eigenfunctions, expactation value and uncertainty of x
     """
-    np.savetxt('potential.dat', np.concatenate((np.reshape(xnew, (1999,1)), np.reshape(potential(xnew),(1999,1))),axis=1))
+    np.savetxt('potential.dat', np.concatenate((np.reshape(xnew, (1999, 1)),
+                                                np.reshape(potential(xnew), (1999, 1))), axis=1))
     np.savetxt('energies.dat', energies[first-1:last])
-    np.savetxt('wavefuncs.dat', np.concatenate((np.reshape(xnew,(1999,1)), np.transpose(wavefuncs[first-1:last])),axis=1))
-    np.savetxt('expvalues.dat', np.concatenate((np.reshape(x_exp, (1999,1)), np.reshape(sigma_x,(1999,1))),axis=1))
+    np.savetxt('wavefuncs.dat', np.concatenate((np.reshape(xnew, (1999, 1)),
+                                                np.transpose(wavefuncs[first-1:last])), axis=1))
+    np.savetxt('expvalues.dat', np.concatenate((np.reshape(x_exp, (1999, 1)),
+                                                np.reshape(sigma_x, (1999, 1))), axis=1))
