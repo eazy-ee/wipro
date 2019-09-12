ff21c29f29482700f3b9a0bef5f03b7485e8e7d0
emanuel <eschlake@uni-bremen.de>
2019-09-12 22:57:23 +0200

    Now pylint score > 9. Added message and exit for nonexistent interpolation method.

--- interpolator.py
+++ interpolator.py
@@ -5,22 +5,30 @@ Created on Tue Sep 10 18:19:02 2019
 @author: Emanuel
 
 """
-
-
+import sys
 
 
 from scipy.interpolate import lagrange as poly
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import interp1d
 #from scipy.interpolate import BarycentricInterpolator as poly
-"""import interpolation methods from scipy. Lagrange is very basic and is numerically unstable. Uncomment last line to use differenct interpolating method"""
+
+#import interpolation methods from scipy. Lagrange is very basic and is numerically unstable.
+#Uncomment last line to use differenct interpolating method
 
 
-def interpolator(x,y,method):
-    """Used to interpolate the Potential from a given set of points using a given method. The method can either be polynomial, linear or cspline (natural cubic spline))."""
-    if method=='polynomial':
-        return poly(x,y)
-    elif method=='linear':
-        return interp1d(x,y,kind='linera')
-    elif method=='cspline':
-        return CubicSpline(x,y,bc_type='natural')
+
+def interpolator(x_sup, y_sup, method):
+    """
+    Used to interpolate the Potential from a given set of points using a given method.
+    The method can either be polynomial, linear or cspline (natural cubic spline)).
+    """
+    if method == 'polynomial':
+        return poly(x_sup, y_sup)
+    elif method == 'linear':
+        return interp1d(x_sup, y_sup, kind='linear')
+    elif method == 'cspline':
+        return CubicSpline(x_sup, y_sup, bc_type='natural')
+    print("\nInterpolation method doesn't exist. Chose either linear, polynomial or cspline.\n")
+    sys.exit(1)
+     
