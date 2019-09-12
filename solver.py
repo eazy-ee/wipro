0e99165d292b14170ead09a40d4fe3392d11410f
emanuel <eschlake@uni-bremen.de>
2019-09-12 22:59:26 +0200

    Now reaches pylint score > 9

--- solver.py
+++ solver.py
@@ -1,40 +1,46 @@
+"""
+Created on Mon Sep  9 13:59:53 2019
+
+@author: Emanuel
+"""
+
 import numpy as np
 from scipy import linalg as LA
 
-def solver(V, M, xMin, xMax, nPoint):
+def solver(potential, mass, x_min, x_max, n_point):
     """
     Script to solve the 1-dimensional, stationary schroedinger equation for a given potential. Returns eigenvalues (energielevels) and normalised wavefunctions.
     """
     #setting x-axis
-    xnew = np.linspace(xMin,xMax,nPoint)
+    xnew = np.linspace(x_min, x_max, n_point)
 
     #delta x
     delta = xnew[1]-xnew[0]
 
     #diagonals of coeff matrix, eigenvalues w and eigenvectors v
-    N = np.array([-1/(2*M*delta**2) for i in range(len(xnew)-1)])
-    H = np.array([1/(M*delta**2)+V(i) for i in xnew])
-    w, v = LA.eigh_tridiagonal(H,N)
+    sub_diagonal = np.array([-1/(2*mass*delta**2) for i in range(len(xnew)-1)])
+    main_diagonal = np.array([1/(mass*delta**2)+potential(i) for i in xnew])
+    eigenvalue, eigenvector = LA.eigh_tridiagonal(main_diagonal, sub_diagonal)
 
     #normalised wavefunction
-    psi = np.array([v[:,i]/np.sqrt(delta) for i in range(0,len(xnew))])
+    wavefunc = np.array([eigenvector[:, i]/np.sqrt(delta) for i in range(0, len(xnew))])
 
-    return w, psi
+    return eigenvalue, wavefunc
 
 
-def exp_values(psi, xMin, xMax, nPoint, first, last):
+def exp_values(wavefunc, x_min, x_max, n_point):
     """
-    Calculating the expectation values and uncertainty of x. Returns $<x>$ and $\sigma_x$
+    Calculating the expectation values and uncertainty of x. Returns $<x>$ and sigma_x
     """
     #setting x-axis
-    xnew = np.linspace(xMin,xMax,nPoint)
+    xnew = np.linspace(x_min, x_max, n_point)
 
     #delta x
     delta = xnew[1]-xnew[0]
 
     #expectation values
-    x_exp= delta*np.array([np.sum(psi[i]*xnew*psi[i]) for i in range(0,len(psi))])
-    xsq_exp= delta*np.array([np.sum(psi*xnew**2*psi) for psi in psi])
+    x_exp = delta*np.array([np.sum(wavefunc[i]*xnew*wavefunc[i]) for i in range(0, len(wavefunc))])
+    xsq_exp = delta*np.array([np.sum(wavefunc*xnew**2*wavefunc) for wavefunc in wavefunc])
 
     #uncertainty
     sigma_x = np.sqrt(xsq_exp-x_exp**2)
