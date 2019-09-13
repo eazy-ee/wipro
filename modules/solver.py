a735949254fc95da457740638e13f482a293ebbe
emanuel <eschlake@uni-bremen.de>
2019-09-13 20:09:42 +0200

    moved to modules

---
+++ modules/solver.py
@@ -0,0 +1,48 @@
+"""
+Created on Mon Sep  9 13:59:53 2019
+
+@author: Emanuel
+"""
+
+import numpy as np
+from scipy import linalg as LA
+
+def solver(potential, mass, x_min, x_max, n_point):
+    """
+    Script to solve the 1-dimensional, stationary schroedinger equation for a given potential. Returns eigenvalues (energielevels) and normalised wavefunctions.
+    """
+    #setting x-axis
+    xnew = np.linspace(x_min, x_max, n_point)
+
+    #delta x
+    delta = xnew[1]-xnew[0]
+
+    #diagonals of coeff matrix, eigenvalues w and eigenvectors v
+    sub_diagonal = np.array([-1/(2*mass*delta**2) for i in range(len(xnew)-1)])
+    main_diagonal = np.array([1/(mass*delta**2)+potential(i) for i in xnew])
+    eigenvalue, eigenvector = LA.eigh_tridiagonal(main_diagonal, sub_diagonal)
+
+    #normalised wavefunction
+    wavefunc = np.array([eigenvector[:, i]/np.sqrt(delta) for i in range(0, len(xnew))])
+
+    return eigenvalue, wavefunc
+
+
+def exp_values(wavefunc, x_min, x_max, n_point):
+    """
+    Calculating the expectation values and uncertainty of x. Returns $<x>$ and sigma_x
+    """
+    #setting x-axis
+    xnew = np.linspace(x_min, x_max, n_point)
+
+    #delta x
+    delta = xnew[1]-xnew[0]
+
+    #expectation values
+    x_exp = delta*np.array([np.sum(wavefunc[i]*xnew*wavefunc[i]) for i in range(0, len(wavefunc))])
+    xsq_exp = delta*np.array([np.sum(wavefunc*xnew**2*wavefunc) for wavefunc in wavefunc])
+
+    #uncertainty
+    sigma_x = np.sqrt(xsq_exp-x_exp**2)
+
+    return x_exp, sigma_x
