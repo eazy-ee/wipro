e76fe67a1382b284646e02d2665f4fe493325405
emanuel <eschlake@uni-bremen.de>
2019-09-13 20:21:31 +0200

    Not defining an additional function anymore. Doesn't visualize anymore.

--- schroedinger.py
+++ schroedinger.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  9 13:59:53 2019
-
-@author: Emanuel
-"""
-#import sys
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-import in_out
-import interpolator
-import solver
-import visualizer
+from modules import in_out, interpolator, solver
 
-def schroedinger(scale, inp):
-    """
-    Solves the 1-dimensional stationary schroedinger equation
-    """
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate the solution to the 1-dimensional Schroedinger equation for a given Potential.')
+    parser.add_argument('-i', '--input', type=str, required=False, help='Path to the schroedinger.inp file')
+    parser.add_argument('-o', '--output', type=str, required=False,  help='Directory of the output files.')
+    args = parser.parse_args()
+
+    if args.input is None:
+        args.input = './schroedinger.inp'
+    if args.output is None:
+       args.output = './'
+
    #parameters set in input file
-    params = in_out.params(inp)
+    params = in_out.params(args.input)
 
    #interpolated POTENTIAL
     potential = interpolator.interpolator(params['x_sup'], params['y_sup'], params['method'])
@@ -36,22 +32,4 @@ def schroedinger(scale, inp):
     x_exp, sigma_x = solver.exp_values(psi, params['x_min'], params['x_max'], params['n_point'])
 
    #saving results to files
-    in_out.output(potential, energies, psi, x_exp, sigma_x, xnew, params['first'], params['last'])
-
-   #visualizing data
-    visualizer.visualizer(psi, potential, energies, x_exp, sigma_x, scale, **params)
-
-
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Calculate and visualize the solution to the 1-dimensional Schroedinger equation for a given Potential.')
-    parser.add_argument('-s', '--scale', type=float, required=False, help='Scaling the wavefunction.')
-    parser.add_argument('-p', '--path', type=str, required=False,  help='Path of the input file.')
-    args = parser.parse_args()
-    if args.scale is None:
-        args.scale = 1.0
-    if args.path is None:
-       args.path = './schroedinger.inp'
-    schroedinger(args.scale, str(args.path))
-    plt.show()
+    in_out.output(potential, energies, psi, x_exp, sigma_x, xnew, params['first'], params['last'], args.output)
