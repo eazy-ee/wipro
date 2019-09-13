5841ca1b2f67d1fbc128006be83cd99ba682ab43
emanuel <eschlake@uni-bremen.de>
2019-09-13 15:32:37 +0200

    Added parser for scale and inputfile path. Now executable

--- schroedinger.py
+++ schroedinger.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep  9 13:59:53 2019
 
 @author: Emanuel
 """
+#import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import in_out
@@ -11,13 +15,12 @@ import interpolator
 import solver
 import visualizer
 
-
-def schroedinger():
+def schroedinger(scale, inp):
     """
     Solves the 1-dimensional stationary schroedinger equation
     """
    #parameters set in input file
-    params = in_out.params()
+    params = in_out.params(inp)
 
    #interpolated POTENTIAL
     potential = interpolator.interpolator(params['x_sup'], params['y_sup'], params['method'])
@@ -36,7 +39,19 @@ def schroedinger():
     in_out.output(potential, energies, psi, x_exp, sigma_x, xnew, params['first'], params['last'])
 
    #visualizing data
-    visualizer.visualizer(psi, potential, energies, x_exp, sigma_x, **params)
+    visualizer.visualizer(psi, potential, energies, x_exp, sigma_x, scale, **params)
 
-schroedinger()
-plt.show()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate and visualize the solution to the 1-dimensional Schroedinger equation for a given Potential.')
+    parser.add_argument('-s', '--scale', type=float, required=False, help='Scaling the wavefunction.')
+    parser.add_argument('-p', '--path', type=str, required=False,  help='Path of the input file.')
+    args = parser.parse_args()
+    if args.scale is None:
+        args.scale = 1.0
+    if args.path is None:
+       args.path = './schroedinger.inp'
+    schroedinger(args.scale, str(args.path))
+    plt.show()
